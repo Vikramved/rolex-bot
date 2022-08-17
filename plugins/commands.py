134c412fb4299f1488e95184f937245fe1abb261
Shahidshibu <99473783+Shahidshibu@users.noreply.github.com>
2022-08-17 23:35:01 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -23,10 +23,10 @@ async def start(client, message):
     if message.chat.type in ['group', 'supergroup']:
         buttons = [
             [
-                InlineKeyboardButton('🎬 Movie Group', url='https://t.me/MovieBoss4')
+                InlineKeyboardButton('🎬 ᴍᴏᴠɪᴇ ɢʀᴏᴜᴩ', url='https://t.me/MovieBoss4')
             ],
             [
-                InlineKeyboardButton('ℹ️ Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
+                InlineKeyboardButton('ℹ️ ʜᴇʟᴩ', url=f"https://t.me/{temp.U_NAME}?start=help"),
             ]
             ]
         reply_markup = InlineKeyboardMarkup(buttons)
@@ -42,11 +42,11 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('👨‍🦱 DEV', url='https://t.me/shahidshibu'),
-            InlineKeyboardButton('🎬 Movie Group', url='https://t.me/MovieBoss4')
+            InlineKeyboardButton('👨‍🦱 ᴏᴡɴᴇʀ', url='https://t.me/shahidshibu'),
+            InlineKeyboardButton('🎬 ᴍᴏᴠɪᴇ ɢʀᴏᴜᴩ', url='https://t.me/MovieBoss4')
             ],[
-            InlineKeyboardButton('ℹ️ Help', callback_data='help'),
-            InlineKeyboardButton('😊 About', callback_data='about')
+            InlineKeyboardButton('ℹ️ ʜᴇʟᴩ', callback_data='help'),
+            InlineKeyboardButton('😊 ᴀʙᴏᴜᴛ', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
@@ -78,11 +78,11 @@ async def start(client, message):
         return
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
-            InlineKeyboardButton('👨‍🦱 DEV', url='https://t.me/shahidshibu'),
-            InlineKeyboardButton('🎥 Backup Group', url='https://t.me/+keg4jBftcVEzOGY9')
+            InlineKeyboardButton('👨‍🦱 ᴅᴇᴠ', url='https://t.me/shahidshibu'),
+            InlineKeyboardButton('🎥 ʙᴀᴄᴋᴜᴩ ɢʀᴏᴜᴩ', url='https://t.me/+keg4jBftcVEzOGY9')
             ],[
-            InlineKeyboardButton('ℹ️ Help', callback_data='help'),
-            InlineKeyboardButton('😊 About', callback_data='about')
+            InlineKeyboardButton('ℹ️ ʜᴇʟᴩ', callback_data='help'),
+            InlineKeyboardButton('😊 ᴀʙᴏᴜᴛ', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
